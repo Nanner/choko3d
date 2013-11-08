6bc94a0e824aa12063f8e2d5535b2636a20f95ab
pedrorfernandes <pedrorfernandes@yahoo.com>
2013-11-08 18:43:03 +0000

    texels are fixed in patch and plane

--- CGFexample/src/Plane.cpp
+++ CGFexample/src/Plane.cpp
@@ -1,10 +1,10 @@
 #include "Plane.h"
 
 GLfloat Plane::ctrlpoints[4][3] = {
-                                    {  0.5, 0.0,  0.5},
                                     { -0.5, 0.0,  0.5},
-                                    {  0.5, 0.0, -0.5},
-                                    { -0.5, 0.0, -0.5}
+                                    {  0.5, 0.0,  0.5},
+                                    { -0.5, 0.0, -0.5},
+                                    {  0.5, 0.0, -0.5}
                                 };
 
 GLfloat Plane::nrmlcompon[4][3] = {
@@ -19,20 +19,34 @@ GLfloat Plane::colorpoints[4][4] = {
                                 { 0.0, 1.0, 0.0, 0},
                                 { 1.0, 0.0, 0.0, 0} };
 
-GLfloat Plane::textpoints[4][2] = {
-                                { 1.0, 0.0},
-                                { 0.0, 0.0},
-                                { 1.0, 1.0},
-                                { 0.0, 1.0}
-};
+
 
 
 Plane::Plane(int parts){
     matrix = NULL;
     this->parts = parts;
+    this->texelsInitialized = false;
+    this->previousFrontFace = new GLint();
+}
+
+void Plane::initializeTexels() {
+    float s = 1.0; float t = 1.0;
+    Appearance* appearance = NULL;
+    if( this->getAppearance() ) {
+		appearance = this->getAppearance();
+        s = 1.0 / appearance->getTexLength_s();
+        t = 1.0 / appearance->getTexLength_t();
+    }
+    
+    texels.push_back(0); texels.push_back(0);
+    texels.push_back(s); texels.push_back(0);
+    texels.push_back(0); texels.push_back(t);
+    texels.push_back(s); texels.push_back(t);
 }
 
 void Plane::draw() {
+    
+    if (!texelsInitialized) initializeTexels();
     
     Appearance* appearance = NULL;
     if( this->getAppearance() ) {
@@ -40,6 +54,8 @@ void Plane::draw() {
         appearance->apply();
     }
     
+    glGetIntegerv(GL_FRONT_FACE, previousFrontFace);
+    glFrontFace(GL_CW);
     
     glEnable(GL_MAP2_VERTEX_3);
 	glEnable(GL_MAP2_NORMAL);
@@ -49,13 +65,14 @@ void Plane::draw() {
     glMap2f(GL_MAP2_VERTEX_3, 0.0, 1.0, 3, 2,  0.0, 1.0, 6, 2,  &Plane::ctrlpoints[0][0]);
 	glMap2f(GL_MAP2_NORMAL,   0.0, 1.0, 3, 2,  0.0, 1.0, 6, 2,  &Plane::nrmlcompon[0][0]);
 	//glMap2f(GL_MAP2_COLOR_4,  0.0, 1.0, 4, 2,  0.0, 1.0, 8, 2,  &Plane::colorpoints[0][0]);
-    glMap2f(GL_MAP2_TEXTURE_COORD_2,  0.0, 1.0, 2, 2,  0.0, 1.0, 4, 2,  &Plane::textpoints[0][0]);
+    glMap2f(GL_MAP2_TEXTURE_COORD_2,  0.0, 1.0, 2, 2,  0.0, 1.0, 4, 2,  &texels[0]);
 
     
 	glMapGrid2f(parts, 0.0, 1.0, parts, 0.0, 1.0);
     
     glEvalMesh2(GL_FILL, 0, parts, 0, parts);
     
+    glFrontFace(*previousFrontFace);
     
     // prints vertex numbers for debugging purposes
     glColor3f(1.0, 1.0, 0.0);
